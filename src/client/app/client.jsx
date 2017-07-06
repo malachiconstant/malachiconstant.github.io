038144732eaed73a41b238b3b77c51d20d612502
malachiconstant <jon.manalo@galepartners.com>
2017-07-06 16:56:09 -0400

    progress meter wip

--- src/client/app/client.jsx
+++ src/client/app/client.jsx
@@ -5,12 +5,14 @@ import { Router, Route, IndexRoute, hashHistory} from 'react-router';
 import "./sass/common.scss";
 import HomePage from "./pages/HomePage.jsx";
 import AboutTheSitePage from "./pages/AboutTheSitePage.jsx";
+import ProgressMeter from "./components/ProgressMeter.jsx";
 
 
 class Client extends React.Component{
 	render() {
 		return(
 			<div ref="mainWrapper" className="main-wrapper">
+				<ProgressMeter />
 				<Router history={ hashHistory }>
 					<Route path="/" component={HomePage}></Route>
 					<Route path="about" component={AboutTheSitePage}></Route>
---
+++ src/client/app/components/ProgressMeter.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {Link, IndexLink} from 'react-router';
+import '../sass/ProgressMeter.scss';
+
+class ProgressMeter extends React.Component {
+	componentDidMount() {
+		const self = this;
+		window.addEventListener('load', function() {
+			progressBar();
+		});
+		function progressBar() {
+			const gauge = self.refs.gauge;
+			const winHeight = window.innerHeight;
+			const winWidth = window.innerWidth;
+			const body = document.body;
+			const html = document.documentElement;
+			const docHeight = (Math.max( body.scrollHeight, body.offsetHeight,html.clientHeight, html.scrollHeight, html.offsetHeight )) - winHeight;
+			const YTop = document.body.scrollTop;
+			const pPercentage = (YTop / docHeight) * 100;
+						console.log(pPercentage);
+
+			gauge.style.width = pPercentage + "%";
+		}
+	}
+	render() {
+		return(
+			<div className="progress-meter">
+				<div ref="gauge" className="gauge">
+				</div>
+			</div>
+		)
+	}
+}
+export default ProgressMeter;
