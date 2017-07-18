905f98215e04f6f963675b31a6d66cdc495162c1
malachiconstant <jon.manalo@galepartners.com>
2017-07-18 17:12:04 -0400

    cross hatch wip 02

--- src/client/app/components/CrossHatch.jsx
+++ src/client/app/components/CrossHatch.jsx
@@ -7,34 +7,62 @@ class CrossHatch extends React.Component {
 		constructor(props) {
 	  super(props);
 	  this.state = {
-
 	  };
 	}
 	componentDidMount() {
-
 	}
 	render() {
-		function numArray(num1) {
-			const num = num1;
-			const numbers = [];
-			for (var i=0; i <= num; i++) {
-			  numbers.push(i);
+		function crossLines(dimension,splitBy) {
+			const totalLines = dimension * splitBy;
+			const round =  Math.round(totalLines);
+			const theArray = [];
+			for (var i=0; i <= round; i++) {
+			  theArray.push(i);
 			}
-			return numbers;
+			return theArray;
 		}
-		const hItems = numArray(this.props.hAmt).map((numbers) =>
-		  <div className="h-lines" key={"h" + numbers}></div>);
-		const vItems = numArray(this.props.vAmt).map((numbers) =>
-		  <div className="v-lines" key={"v" + numbers}></div>);
 
+		const hItems = crossLines(window.innerHeight,0.05).map((number) =>
+			<div className="h-lines" 
+				style={{
+					width: ((this.props.menuOpen) ? 100 : 0) + "%",
+					transition: "width 0.5s",
+					transitionDelay: Math.round(100*(number * 0.02))/100 + "s"
+				}}
+				key={"h" + number}>
+			</div>
+		);
+
+		const vItems = crossLines(window.innerWidth,0.05).map((number) =>
+			<div className="v-lines" 
+				style={{
+					height: ((this.props.menuOpen) ? window.innerHeight : 0) + "px",
+					transition: "height 0.5s",
+					transitionDelay: Math.round(100*(number * 0.01))/100 + "s"
+				}}
+				key={"v" + number}>
+			</div>
+		);
+		const lastLine = Math.max(
+							Math.round(100*((crossLines(window.innerWidth,0.05).length)*0.01))/100,
+							Math.round(100*((crossLines(window.innerHeight,0.15).length)*0.01))/100);
+		const crossHatchStyle = {
+			transitionDelay: ((this.props.menuOpen) ? 0 : lastLine*1.5) + "s"
+		}
+		const bgStyle = {
+			transitionDelay: ((this.props.menuOpen) ? 0 : lastLine*1) + "s"
+		}
 		return(
-			<div className="crosshatch-container">
+			<div className={"crosshatch-container " + ((this.props.menuOpen) ? "open": "")} style={crossHatchStyle}>
+				<div className="cross-hatch-bg" style={bgStyle}></div>
 				<div className="h-container">
 					{hItems}
 				</div>
+
 				<div className="v-container">
 					{vItems}
 				</div>
+
 			</div>
 		)
 	}
--- src/client/app/components/MainMenu.jsx
+++ src/client/app/components/MainMenu.jsx
@@ -5,20 +5,34 @@ import CrossHatch from "../components/CrossHatch.jsx";
 import '../sass/MainMenu.scss';
 
 class MainMenu extends React.Component {
+		constructor(props) {
+	  super(props);
+	  this.state = {
+	  	menuOpen : false
+	  };
+	   this._toggleMenu = this._toggleMenu.bind(this);
+	}
 	componentDidMount() {
 
+
+	}
+	_toggleMenu(){
+		console.log("hellowwww");
+		this.setState(prevState => ({menuOpen : !prevState.menuOpen}));
 	}
 	render() {
+
 		return(
 			<div className="main-menu">
+			<p>something will go here</p>
 				<div className="mobile-wrapper">
 					<div className="top-bar">
 						<div className="main-logo"></div>
-						<div className="menu-button" role="button" aria-haspopup="true" tabIndex="0">x</div>
+						<div onClick={() => this._toggleMenu()} className="menu-button" role="button" aria-haspopup="true" tabIndex="0">x</div>
 					</div>
 				</div>
 				<CrossHatch 
-					hAmt={100} vAmt={50}
+				menuOpen={this.state.menuOpen}
 				/>
 			</div>
 		)
