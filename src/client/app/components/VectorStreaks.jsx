24db76b589bbe5ebc3efa14a1c5512b0240d4b26
malachiconstant <jon.manalo@galepartners.com>
2017-08-03 16:08:10 -0400

    closer but not really

--- src/client/app/components/VectorStreaks.jsx
+++ src/client/app/components/VectorStreaks.jsx
@@ -3,6 +3,10 @@ import {render} from 'react-dom';
 import {Link, IndexLink} from 'react-router';
 import '../sass/VectorStreaks.scss';
 
+
+
+
+
 class VectorStreaks extends React.Component {
 		constructor(props) {
 	  super(props);
@@ -12,87 +16,113 @@ class VectorStreaks extends React.Component {
 
 	}
 	componentDidMount() {
-		const self = this;
-		let perSquareArea = 90000;
 
-		var lastPos = null;
-		var scrollSpeed = 0;
+	}	
 
-		this._svgContainerDims();
-		this._generateAmount(perSquareArea);
-		 
-		window.addEventListener("resize", function(){			
-			self._svgContainerDims();
-			self._generateAmount(perSquareArea);
 
-		});
-		window.addEventListener("scroll", function(){			
-			// self._checkScrollSpeed();
-			checkScrollSpeed();
 
-		});
-		function checkScrollSpeed(){
-			const newPos = window.scrollY;
-			if( lastPos != null){
-				scrollSpeed = newPos - lastPos;
-				clearTimeout(timer);
-			}
-			const timer = setTimeout(function(){
-				lastPos = newPos;
-			}, 50);
-			return scrollSpeed;
-		}
 
-	}
-		_svgContainerDims() {
-			const svgContainer = this.refs.svgContainer;
-			svgContainer.setAttribute("width", document.documentElement.clientWidth);
-			svgContainer.setAttribute("height", document.body.offsetHeight);
-		}
-		_generateAmount(perSquareArea) {
-			const docArea = document.documentElement.clientWidth * document.body.offsetHeight;
-			const rawAmountPerArea = docArea/perSquareArea;
-			const roundedAmountPerArea = Math.ceil(rawAmountPerArea/100)*100;
-			this.setState({ vectorAmount: roundedAmountPerArea });
-		}	
-
-	_createVectors() {
-
-		const vW = document.documentElement.clientWidth;
-		const vH = document.body.offsetHeight;
-		const vectorAmount = this.state.vectorAmount;
-		const theArray = [];
-		for (var i=0; i <= vectorAmount; i++) {
-		  theArray.push({
-		  	rW : Math.floor(Math.random() * 400)/100,
-		  	vW : Math.floor(Math.random() * vW),
-		  	vH : Math.floor(Math.random() * vH),
-		  	rH : 200
-		  });
-		}
-		return theArray;		
-	}
 	render() {
-		const self = this;
-		const vectorLines = self._createVectors().map((item, number) =>
-			<path
-				className="vector-streak"
-				ref="pathref"
-				key={"pathkey" + number}
-				id={"pathID" + number}
-				d={"M " + item.vW + " " + item.vH + " h " + item.rW + " v " + item.rH + " h -" + item.rW + " Z"}
-				stroke="none"
-  				strokeWidth="0"
-  				fill="#000000" />
-			
-		)
+		
+		setTimeout(function(){
+			const svgContainer = document.getElementById('svgContainer');
+
+			svgContainerDims();
+			createRect();
+
+				function svgContainerDims() {
+					svgContainer.setAttribute("width", document.documentElement.clientWidth);
+					svgContainer.setAttribute("height", document.body.offsetHeight);
+				}
+
+				function createRect() {
+
+						const vX = document.documentElement.clientWidth;
+						const vY = document.body.offsetHeight;
+
+						let lastPos = null;
+						let scrollSpeed = 0;
+
+						const svgContainer = document.getElementById('svgContainer');
+						let svgRect = document.getElementsByTagName("rect");
+
+
+
+					generateRect();
+
+
+
+					function generateAmount(perSquareArea) {
+						const docArea = document.documentElement.clientWidth * document.body.offsetHeight;
+						const rawAmountPerArea = docArea/perSquareArea;
+						const roundedAmountPerArea = Math.round(rawAmountPerArea);
+
+						return roundedAmountPerArea;
+					}
+
+					function randomizePos(axis) {
+						  	// Math.floor(Math.random() * 400)/100;
+						  	return Math.floor(Math.random() * axis);
+					}
+
+					function randomizeWidth() {
+						return Math.floor(Math.random() * 400)/100;
+					}
+
+					function vectorSpeed() {
+
+							const newPos = window.scrollY;
+							if(lastPos != null) {
+								scrollSpeed = newPos - lastPos;
+								clearTimeout(timer);
+							}
+
+							const timer = setTimeout(function(){
+								lastPos = newPos;
+							}, 50);	
+							console.log(scrollSpeed);
+
+							return scrollSpeed;
+							
+
+					}
+
+					function generateRect(){
+
+
+						for (var i = 0; i < generateAmount(100000); i++) {
+							// console.log("hiiii " + i);
+							var rX = randomizePos(vX);
+							var rY = randomizePos(vY);
+							svgContainer.appendChild(document.createElementNS("http://www.w3.org/2000/svg","rect"));
+							svgRect[i].setAttributeNS(null,"x", randomizePos(vX));
+							svgRect[i].setAttributeNS(null,"y", randomizePos(vY));
+							svgRect[i].setAttributeNS(null,"width", randomizeWidth());
+							svgRect[i].setAttributeNS(null,"height", "300");
+							svgRect[i].style.fill = '#000000';
+
+						}
+						window.addEventListener('scroll',function(){
+							for (var i = 0; i < generateAmount(100000); i++) {
+								svgRect[i].setAttributeNS(null,"height", Math.abs(vectorSpeed()));
+
+								
+							}
+						});
+					} 
+
+					// svgPath[0].setAttribute("d", "M " + window.innerWidth/2 + " " + document.body.offsetHeight/2 + " h 10 v " + vectorSpeed() + " h -10 Z");
+				}
+		},25);
+
 		return(
-			<div className="vector-streaks-container">
-				<svg ref="svgContainer" className="svg-container">
-					{vectorLines}
+			<div id="theVContainer" className="vector-streaks-container">
+				<svg id="svgContainer" ref="svgContainer" className="svg-container">
+					
 				</svg>
 			</div>
 		)
 	}
+
 }
 export default VectorStreaks;
