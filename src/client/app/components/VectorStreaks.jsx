3a2669675c2e7eefcfacfc070b32790eeaed948a
malachiconstant <jon.manalo@galepartners.com>
2017-08-10 16:14:00 -0400

    working vector streak

--- src/client/app/components/VectorStreaks.jsx
+++ src/client/app/components/VectorStreaks.jsx
@@ -87,10 +87,6 @@ class VectorStreaks extends React.Component {
 						const vY = document.body.offsetHeight;
 						const rW = Math.floor(Math.random() * 400)/100;
 
-
-
-
-
 						function lineInstance(number) {
 							const vX = document.documentElement.clientWidth;
 							const vY = document.body.offsetHeight;
@@ -98,14 +94,14 @@ class VectorStreaks extends React.Component {
 							svgContainer.appendChild(document.createElementNS("http://www.w3.org/2000/svg","line"));
 							svgLine[number].setAttributeNS(null,"data-x", randomizePos(vX));
 							svgLine[number].setAttributeNS(null,"data-y", randomizePos(vY));
-							svgLine[number].setAttributeNS(null,"data-y2", 0);
+							svgLine[number].setAttributeNS(null,"data-yr", parseInt(Math.random() * (400 - 100) + 100));
 							svgLine[number].setAttributeNS(null,"x1", svgLine[number].dataset.x);
 							svgLine[number].setAttributeNS(null,"y1", svgLine[number].dataset.y);
 							svgLine[number].setAttributeNS(null,"x2", svgLine[number].dataset.x);
 							svgLine[number].setAttributeNS(null,"y2", svgLine[number].dataset.y);
 							svgLine[number].setAttributeNS(null,"stroke", "#000000" );
-							svgLine[number].setAttributeNS(null,"stroke-width", Math.floor(Math.random() * 400)/100);
-							svgLine[number].style.opacity = Math.random();
+							svgLine[number].setAttributeNS(null,"stroke-width", Math.floor(Math.random() * (5 - 1) + 1));
+							svgLine[number].style.opacity = Math.random() * (0.5 - 0.1) + 0.1;
 
 						}
 						for (var i = 0; i < generateAmount(100000); i++) {
@@ -124,8 +120,36 @@ class VectorStreaks extends React.Component {
 						window.addEventListener('scroll',function(){
 							for (var i = 0; i < generateAmount(100000); i++) {
 								const eachLine = svgLine[i];
-								TweenMax.from(eachLine, 1, {attr:{y2: ((vectorSpeed()/rW) + parseInt(svgLine[i].dataset.y))}, ease:Linear.easeNone});
-								TweenMax.to(eachLine, 1, {attr:{y2: parseInt(svgLine[i].dataset.y)}, ease:Power2.easeIn});
+								// TweenMax.from(eachLine, 1, {attr:{y2: (vectorSpeed() + parseInt(eachLine.dataset.y))}, ease:Linear.easeNone});
+
+								if( (vectorSpeed() + parseInt(eachLine.dataset.y)) > Number(eachLine.dataset.yr/4) + parseInt(eachLine.dataset.y)){
+
+									TweenMax.from(eachLine, eachLine.dataset.yr/500, {attr:{y2: (Number(eachLine.dataset.yr/4) + parseInt(eachLine.dataset.y))}, ease:Linear.easeNone});
+								};
+								if( (vectorSpeed() + parseInt(eachLine.dataset.y)) < -Number(eachLine.dataset.yr/4) + parseInt(eachLine.dataset.y)) {
+				
+									TweenMax.from(eachLine, eachLine.dataset.yr/500, {attr:{y2: (-Number(eachLine.dataset.yr/4) + parseInt(eachLine.dataset.y))}, ease:Linear.easeNone});
+								};
+
+								if( (vectorSpeed() + parseInt(eachLine.dataset.y)) > Number(eachLine.dataset.yr/2) + parseInt(eachLine.dataset.y)){
+
+									TweenMax.from(eachLine, eachLine.dataset.yr/500, {attr:{y2: (Number(eachLine.dataset.yr/2) + parseInt(eachLine.dataset.y))}, ease:Linear.easeNone});
+								};
+								if( (vectorSpeed() + parseInt(eachLine.dataset.y)) < -Number(eachLine.dataset.yr/2) + parseInt(eachLine.dataset.y)) {
+				
+									TweenMax.from(eachLine, eachLine.dataset.yr/500, {attr:{y2: (-Number(eachLine.dataset.yr/2) + parseInt(eachLine.dataset.y))}, ease:Linear.easeNone});
+								};
+
+								if( (vectorSpeed() + parseInt(eachLine.dataset.y)) > Number(eachLine.dataset.yr) + parseInt(eachLine.dataset.y)){
+
+									TweenMax.from(eachLine, eachLine.dataset.yr/500, {attr:{y2: (Number(eachLine.dataset.yr) + parseInt(eachLine.dataset.y))}, ease:Linear.easeNone});
+								};
+								if( (vectorSpeed() + parseInt(eachLine.dataset.y)) < -Number(eachLine.dataset.yr) + parseInt(eachLine.dataset.y)) {
+				
+									TweenMax.from(eachLine, eachLine.dataset.yr/500, {attr:{y2: (-Number(eachLine.dataset.yr) + parseInt(eachLine.dataset.y))}, ease:Linear.easeNone});
+								};
+
+								TweenMax.to(eachLine, eachLine.dataset.yr/500, {attr:{y2: parseInt(eachLine.dataset.y)}, ease:Power1.easeOut});
 							}
 						});
 
